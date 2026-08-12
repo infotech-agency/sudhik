09897c50a4db2c7fc5aa00445fd247475020eab5
infotech <infoconwayy@gmail.com>
2026-08-12 14:49:51 +0530

    something

--- next-sudhik/project/src/lib/api.ts
+++ next-sudhik/project/src/lib/api.ts
@@ -3,7 +3,7 @@
 // { success, data } / { message }.
 
 const BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || 'https://api.shuddhik.com';
+   'https://api.shuddhik.com';
 
 const TOKEN_KEY = 'shuddhik_token';
 
