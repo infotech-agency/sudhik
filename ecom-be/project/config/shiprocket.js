49aa50ac78b86532f855270be2edbcb16fc5bfb8
infotech <infoconwayy@gmail.com>
2026-08-21 10:59:59 +0530

    shiprocket

--- ecom-be/project/config/shiprocket.js
+++ ecom-be/project/config/shiprocket.js
@@ -10,9 +10,17 @@
  *  SHIPROCKET_BASE_URL  - https://apiv2.shiprocket.in (production)
  *  SHIPROCKET_PICKUP_LOCATION - The pickup location name registered in Shiprocket
  */
+// module.exports = {
+//   email: process.env.SHIPROCKET_EMAIL || '',
+//   password: process.env.SHIPROCKET_PASSWORD || '',
+//   baseUrl: process.env.SHIPROCKET_BASE_URL || 'https://apiv2.shiprocket.in',
+//   pickupLocation: process.env.SHIPROCKET_PICKUP_LOCATION || 'Primary',
+// };
+
 module.exports = {
-  email: process.env.SHIPROCKET_EMAIL || '',
-  password: process.env.SHIPROCKET_PASSWORD || '',
+  email: 'shuddhik.shiprocket.api@gmail.com',
+  password:  'ysyqxxMpjEGQUR&2iF8&!cTPPZeS5txv',
   baseUrl: process.env.SHIPROCKET_BASE_URL || 'https://apiv2.shiprocket.in',
   pickupLocation: process.env.SHIPROCKET_PICKUP_LOCATION || 'Primary',
 };
+
